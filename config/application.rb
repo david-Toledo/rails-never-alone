81aa9ee7cc973cfe00d30733a7f27a4eeb49ce37
david-Toledo <davidtoledo87@hotmail.com>
2021-02-03 10:10:59 +1100

    precompile fixed

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,8 @@ require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-config.assets.initialize_on_precompile = false
-
+# config.assets.initialize_on_precompile = false
+# RAILS_ENV=production bundle exec rake assets:precompile
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
